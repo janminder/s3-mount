9f1913dc5226652d46be2f252ea327c9d980dfc9
Jan Minder <minderjan@gmail.com>
2017-01-05 14:40:03 +0100

    Removed Cred Command

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Get s3 creds from env var and write to password file
-echo "$S3_IDENTITY":"$S3_CREDENTIAL" >> /etc/passwd-s3fs
-chmod 600 /etc/passwd-s3fs
 # Mount s3 bucket from environment variable
 mkdir -p $S3_LOCATION
-s3fs $S3_BUCKET $S3_LOCATION
+s3fs $S3_BUCKET $S3_LOCATION -o url=$S3_ENDPOINT_URL
 
 # Keep it running
 tail -f /dev/null
